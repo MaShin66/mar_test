1355b0f2e4d87a11f41bc54fc22286d303ff3fcd
신호태 <ubiquitous6666@gmail.com>
2019-09-07 21:10:56 +0900

    Updates

---
+++ precache-manifest.ee74c9b9cfe4f17aab3cb74cc3e556b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f749a42f911154a06b911832b4c2dc1",
+    "url": "/mar_test/index.html"
+  },
+  {
+    "revision": "ea6748d6a168a5f35680",
+    "url": "/mar_test/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "62a9235e45b6e36c2450",
+    "url": "/mar_test/static/js/2.3064a71b.chunk.js"
+  },
+  {
+    "revision": "ea6748d6a168a5f35680",
+    "url": "/mar_test/static/js/main.b11b16e4.chunk.js"
+  },
+  {
+    "revision": "69e51e3f8459280d9b0f",
+    "url": "/mar_test/static/js/runtime~main.15e7985c.js"
+  }
+]);
